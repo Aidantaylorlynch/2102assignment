8e3482c75543379db05e3eb1e3b9e3703cf9f95b
Aidan <aidan@dyn-118-138-3-193.its.monash.edu.au>
2019-09-09 14:27:49 +1000

    get player movement correct

--- asteroids.js
+++ asteroids.js
@@ -1,11 +1,66 @@
 "use strict";
 function asteroids() {
+    class Shippyboy {
+        constructor(ship) {
+            this.rotate = 0;
+            this.x = 300;
+            this.y = 300;
+            this.shippyboy = ship;
+        }
+        setPossy() {
+            const possy = "translate(" + this.x + " " + this.y + ") " + "rotate(" + this.rotate + ")";
+            this.shippyboy.attr("transform", possy.toString());
+        }
+        toRadians(angle) {
+            return angle * (Math.PI / 180);
+        }
+        move() {
+            console.log("current rotation", this.rotate);
+            console.log("current rotation mod", this.rotate % 360);
+            console.log("current x", this.x);
+            console.log("current y", this.y);
+            console.log("x rad", this.toRadians(this.x));
+            console.log("y rad", this.toRadians(this.y));
+            const adjrot = this.rotate % 360;
+            console.log("sin", Math.sin(this.toRadians(adjrot)));
+            console.log("cos", Math.cos(this.toRadians(adjrot)));
+            const adjx = Math.sin(this.toRadians(adjrot));
+            const adjy = Math.cos(this.toRadians(adjrot));
+            const moveX = 10 * adjx;
+            const moveY = -1 * 10 * adjy;
+            console.log(moveX);
+            console.log(moveY);
+            this.x += moveX;
+            this.y += moveY;
+            this.setPossy();
+        }
+        setRotate(newRot) {
+            this.rotate = this.rotate + newRot;
+            this.setPossy();
+        }
+    }
     const svg = document.getElementById("canvas");
     let g = new Elem(svg, 'g')
-        .attr("transform", "translate(300 300) rotate(170)");
+        .attr("transform", "translate(300 300) rotate(180)");
     let ship = new Elem(svg, 'polygon', g.elem)
         .attr("points", "-15,20 15,20 0,-20")
         .attr("style", "fill:lime;stroke:purple;stroke-width:1");
+    const shippyBoy = new Shippyboy(g);
+    Observable.fromEvent(document, 'keydown')
+        .map((event) => {
+        return event.key;
+    })
+        .subscribe((keyCode) => {
+        if (keyCode == "ArrowRight") {
+            shippyBoy.setRotate(20);
+        }
+        else if (keyCode == "ArrowLeft") {
+            shippyBoy.setRotate(-20);
+        }
+        else if (keyCode == "ArrowUp") {
+            shippyBoy.move();
+        }
+    });
 }
 if (typeof window != 'undefined')
     window.onload = () => {
--- asteroids.ts
+++ asteroids.ts
@@ -2,6 +2,52 @@
 // https://docs.google.com/document/d/1Gr-M6LTU-tfm4yabqZWJYg-zTjEVqHKKTCvePGCYsUA/edit?usp=sharing
 
 function asteroids() {
+
+  class Shippyboy {
+    shippyboy: Elem;
+    rotate: number = 0;
+    x: number = 300;
+    y: number = 300;
+
+    constructor(ship: Elem) {
+      this.shippyboy = ship;
+    }
+
+    setPossy() {
+      const possy = "translate(" + this.x + " " + this.y + ") " + "rotate(" + this.rotate + ")";
+      this.shippyboy.attr("transform", possy.toString());
+    }
+
+    toRadians(angle: number) {
+      return angle * (Math.PI / 180);
+    }
+
+    move() {
+      console.log("current rotation", this.rotate)
+      console.log("current rotation mod", this.rotate % 360)
+      console.log("current x", this.x)
+      console.log("current y", this.y)
+      console.log("x rad", this.toRadians(this.x))
+      console.log("y rad", this.toRadians(this.y))
+      const adjrot = this.rotate % 360;
+      console.log("sin", Math.sin(this.toRadians(adjrot)))
+      console.log("cos", Math.cos(this.toRadians(adjrot)))
+      const adjx = Math.sin(this.toRadians(adjrot))
+      const adjy = Math.cos(this.toRadians(adjrot))
+      const moveX = 10 * adjx;
+      const moveY = -1 * 10 * adjy
+      console.log(moveX)
+      console.log(moveY)
+      this.x += moveX;
+      this.y += moveY;
+      this.setPossy();
+    }
+
+    setRotate(newRot: number) {
+      this.rotate = this.rotate + newRot;
+      this.setPossy();
+    }
+  }
   // Inside this function you will use the classes and functions 
   // defined in svgelement.ts and observable.ts
   // to add visuals to the svg element in asteroids.html, animate them, and make them interactive.
@@ -16,12 +62,28 @@ function asteroids() {
   // make a group for the spaceship and a transform to move it and rotate it
   // to animate the spaceship you will update the transform property
   let g = new Elem(svg,'g')
-    .attr("transform","translate(300 300) rotate(170)")  
+    .attr("transform","translate(300 300) rotate(180)")  
   
   // create a polygon shape for the space ship as a child of the transform group
   let ship = new Elem(svg, 'polygon', g.elem) 
     .attr("points","-15,20 15,20 0,-20")
     .attr("style","fill:lime;stroke:purple;stroke-width:1")
+  
+  const shippyBoy = new Shippyboy(g);
+
+  Observable.fromEvent<KeyboardEvent>(document, 'keydown')
+    .map((event) => {
+      return event.key;
+    })
+    .subscribe((keyCode) => {
+      if(keyCode == "ArrowRight") {
+        shippyBoy.setRotate(20);
+      } else if (keyCode == "ArrowLeft") {
+        shippyBoy.setRotate(-20);
+      } else if (keyCode == "ArrowUp") {
+        shippyBoy.move();
+      }
+    });
 }
 
 // the following simply runs your asteroids function on window load.  Make sure to leave it in place.
